6f5db56faf5279aa09116f23331ee005c94f31d6
Nat Epstein <nathaniel.epstein@gmail.com>
2021-12-14 15:47:48 -0500

    added boilerplate code to server.js. Requiring sequelize and express npms plus routes folder. Enabled middleware to parse code. Connecting to database and server

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
+//requiring access to express npm, the routes folder and to sequelize
 const express = require('express');
 const routes = require('./routes');
-// TODO: import sequelize connection
+const sequelize = require('./config/connection')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//middleware to parse code
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// TODO: sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+
+// We are connecting to the database through sequelize and connecting to the port as well
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log(`App listening on port ${PORT}!`));
 });
