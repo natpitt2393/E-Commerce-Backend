dc7eba15d94d152850172f38dedb53debc72b4bc
Nat Epstein <nathaniel.epstein@gmail.com>
2021-12-15 08:23:19 -0500

    post method added so that user can successfully create a new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -40,7 +40,15 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json({
+      message: 'You have successfully added a new category!',
+      data: categoryData,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
