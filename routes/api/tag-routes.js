e443142ef9dd65377bdad4c1a607b2771d356226
Nat Epstein <nathaniel.epstein@gmail.com>
2021-12-15 08:11:44 -0500

    tag can now be created using post method

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,7 +41,15 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json({
+      message: "You have successfully created a new tag!",
+      data: tagData,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
