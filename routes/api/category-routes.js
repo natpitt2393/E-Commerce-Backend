1f5b65a2e71e6761a73ff0e6f55850b77b8011fb
Nat Epstein <nathaniel.epstein@gmail.com>
2021-12-15 08:18:53 -0500

    all category data can be accessed through get method

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,39 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get ('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [
+        {
+          model: Product,
+        },
+      ],
+      order: [["id", "DSC"]],
+    });
+    res.status(200).json({
+      message: "Success! Category data retrieved!",
+      data: categoryData,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+ 
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  
 });
 
 module.exports = router;
